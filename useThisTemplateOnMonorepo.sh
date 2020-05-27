131653f403af2f6142e747697dd20863d651cd16
Thallysson Klein <thallyssonklein@gmail.com>
2020-05-27 17:25:07 -0300

    Update useThisTemplateOnMonorepo.sh

--- useThisTemplateOnMonorepo.sh
+++ useThisTemplateOnMonorepo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://codeload.github.com/ThallyssonKlein/EmailAuthAppTemplate/zip/master
+wget "https://codeload.github.com/ThallyssonKlein/EmailAuthAppTemplate/zip/master"
 unzip EmailAuthAppTemplate-master.zip
 rm EmailAuthAppTemplate-master.zip
